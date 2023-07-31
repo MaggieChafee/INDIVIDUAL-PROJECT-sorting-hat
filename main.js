14287160d78e4bc88280adff481cd0c6a9b24e5a
MaggieChafee <maggie.chafee@gmail.com>
2023-07-31 18:45:57 -0500

    created student array, student cards, and updated the sorting hat to a card

--- main.js
+++ main.js
@@ -1,1 +1,43 @@
-console.log("I'm Here")
+students = [
+  {
+    id: 1,
+    name: "Ron Weasley",
+    house: "Gryffindor" 
+  }, 
+  {
+    id: 2,
+    name: "Neville Longbottom",
+    house: "Gryffindor" 
+  }, 
+  {
+    id: 3,
+    name: "Luna Lovegood",
+    house: "Ravenclaw" 
+  }, 
+  {
+    id: 4,
+    name: "Draco Malfoy",
+    house: "Slytherin" 
+  }, 
+]
+
+const renderToDom = (divId, htmlToRender) => {
+  const selectedDiv = document.querySelector(divId);
+  selectedDiv.innerHTML = htmlToRender;
+};
+
+const studentsOnDom = (array) => {
+  let studentString = "";
+  for (const student of array) {
+    studentString += `<div class="card" style="width: 18rem;">
+    <div class="card-body">
+      <h5 class="card-title">${student.name}</h5>
+      <p class="card-text">${student.house}</p>
+      <button type="button" class="btn btn-danger">EXPEL!</button>
+    </div>
+  </div>`
+  } 
+  renderToDom("#studentsContainer", studentString)
+}
+
+studentsOnDom(students)
